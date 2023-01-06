c1f699af51c6f93b77eb533e9deb2e15cf48f5d5
pootytang <delane.jackson@gmail.com>
2023-01-06 13:36:07 -0800

    Added Firebase to nuxt plugin and config

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss'],
+  runtimeConfig: {
+    // The private keys which are only available within server-side
+    FIREBASE_API_KEY: process.env.FB_API_KEY,
+    // Keys within public, will be also exposed to the client-side
+    public: {
+      FB_API_KEY: process.env.FB_API_KEY,
+      FB_APP_ID: process.env.FB_APP_ID,
+    }
+  }
+})
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h3>A BASIC VUE</h3>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
