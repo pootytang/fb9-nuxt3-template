faa8f459205aa076264f1ef7900c105d27c18461
pootytang <delane.jackson@gmail.com>
2023-01-06 15:57:31 -0800

    Added FB SignIn, SignOut, and SignUp functionality

---
+++ composables/useFirebase.ts
@@ -0,0 +1,48 @@
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+
+export const createUser = async (email: string, password: string) => {
+  const auth = getAuth();
+  const credentials = await createUserWithEmailAndPassword(auth, email, password)
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+
+    return credentials
+}
+
+export const signInUser = async (email: string, password: string) => {
+  const auth = getAuth();
+  const credentials = await signInWithEmailAndPassword(auth, email, password)
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+    return credentials
+}
+
+export const signOutUser = async () => {
+  const auth = getAuth();
+  const result = await auth.signOut()
+  console.log(`useFirebase.signOutUser() -> Result: ${result}`)
+  return result
+}
+
+export const initUser = async () => {
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      const uid = user.uid;
+      console.log(user)
+    } else {}
+  });
+}
+
+
+
+
+
+  
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,32 @@
 <template>
   <div>
-    <h3>A BASIC VUE</h3>
+    <button class="btn-primary" @click="signIn">Sign In</button>
+    <button class="btn-primary" @click="signOut">Sign Out</button>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const signIn = async () => {
+  const email = "delane@testuser.test";
+  const password = "password";
+  const creds = await signInUser(email, password);
+  console.log("index --> Credentials: ", creds);
+};
+
+const signOut = async () => {
+  const res = await signOutUser();
+  console.log(`index--> Singed Out Result: ${res}`);
+};
+
+onMounted(async () => {
+  // SIGN UP
+  // const email = "delane@testuser.test";
+  // const password = "password";
+  // const creds = await createUser(email, password);
+  // console.log("index --> Credentials: ", creds);
+  //SIGN IN
+  //SIGN OUT
+});
+</script>
 
 <style lang="scss" scoped></style>
