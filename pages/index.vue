2c4805f926b5615a49e87a474e2d47951b7a1c46
pootytang <delane.jackson@gmail.com>
2023-01-06 20:15:07 -0800

    Cleaned up index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,31 +2,25 @@
   <div>
     <button class="btn-primary" @click="signIn">Sign In</button>
     <button class="btn-primary" @click="signOut">Sign Out</button>
+    <pre>
+    {{ creds }}
+    </pre>
   </div>
 </template>
 
 <script setup>
+const creds = ref();
 const signIn = async () => {
   const email = "delane@testuser.test";
   const password = "password";
-  const creds = await signInUser(email, password);
+  creds.value = await signInUser(email, password);
   console.log("index --> Credentials: ", creds);
 };
 
 const signOut = async () => {
-  const res = await signOutUser();
-  console.log(`index--> Singed Out Result: ${res}`);
+  creds.value = await signOutUser();
+  console.log(`index--> Singed Out Result: ${creds.value}`);
 };
-
-onMounted(async () => {
-  // SIGN UP
-  // const email = "delane@testuser.test";
-  // const password = "password";
-  // const creds = await createUser(email, password);
-  // console.log("index --> Credentials: ", creds);
-  //SIGN IN
-  //SIGN OUT
-});
 </script>
 
 <style lang="scss" scoped></style>
